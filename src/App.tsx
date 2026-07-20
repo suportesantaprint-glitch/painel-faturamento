00dcb275882e40bbc0984ad0865b6a578d6689ae
suportesantaprint-glitch <suporte.santaprint@gmail.com>
2026-07-20 17:52:37 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
 import React, { FormEvent, useEffect, useMemo, useState } from "react";
 import "./App.css";
-import { supabase } from "./supabase";
+import { useState, useEffect } from 'react'
+import { supabase } from './utils/supabase'
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    async function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+  )
+}
 
 type Faturamento = {
   id: number;
