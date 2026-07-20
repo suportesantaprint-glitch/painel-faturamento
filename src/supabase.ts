39715b3f4f5a67f97423dd45be65f2024463b0c6
suportesantaprint-glitch <suporte.santaprint@gmail.com>
2026-07-20 17:34:55 -0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.POSTGRES_HOST as string;
+const supabaseAnonKey = process.env.POSTGRES_PASSWORD as string;
 
 export const supabase = createClient(
   supabaseUrl,
